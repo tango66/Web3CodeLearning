562f2d699a6616777ad594d562af1d522820544b
tango66 <121211709+tango66@users.noreply.github.com>
2023-12-24 21:41:01 +0800

    Create 3 Primitive_data_types.sol

---
+++ Remin Coding Practice/3 Primitive_data_types.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract Premitives{
+    bool public Newbool = false;
+    
+    int public Newint = 1;
+    int public Newint2 = -100;
+
+    uint public Newuint = 0;
+    uint public Newuint2 = 2**23;
+
+    address public Newaddress = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+
+// default value
+    bool public bool1; // false
+    int public int1; // 0
+    uint public uint1; // 0
+    address public address1; // 0x0000000000000000000000000000000000000000
+}
